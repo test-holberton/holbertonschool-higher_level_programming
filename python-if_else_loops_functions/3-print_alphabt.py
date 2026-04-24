f41693bcfdc877a5c0982c6df53d3d3f10e9cd6b
test-holberton <13641@holbertonstudents.com>
2026-04-24 15:45:25 +0400

    feat

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if i != ord("e") and i != ord("q"):
-        print("{}".format(chr(i)), end="")
+        print("{}".format(chr(i)), end = "")
      
